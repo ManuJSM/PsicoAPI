51648f537819fd92b85733bd131fda28cddcf2fd
Manu <ManuJSM@users.noreply.github.com>
2026-01-02 11:56:51 +0100

    prismadbConf

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv'
+const NODE_ENV = process.env['NODE_ENV'] ?? 'development'
+
+dotenv.config({
+  path: NODE_ENV === 'development' ? '.env.local' : '.env'
+})
+
 export const Config = {
   PORT: process.env['PORT'] ?? '3000',
-  NODE_ENV: process.env['NODE_ENV'] ?? 'development'
+  NODE_ENV,
+  DATABASE_SCHEMA: process.env['DATABASE_SCHEMA'],
+  DATABASE_URL: process.env['DATABASE_URL']
 }
